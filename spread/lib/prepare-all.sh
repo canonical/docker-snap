9097852dfd132ac3513d16649784313dcc360b73
Ian Johnson <ian.johnson@canonical.com>
2018-09-21 14:22:32 -0500

    snapcraft.yaml: setup layouts entry for /etc/docker inside the snap
    Also need the spread test's prepare-all to enable snaps with layouts for now, as layouts is still experimental.

--- spread/lib/prepare-all.sh
+++ spread/lib/prepare-all.sh
@@ -11,6 +11,9 @@ newgrp docker
 # make sure that snapd is installed and available
 snap install hello-world
 
+# enable layouts
+sudo snap set core experimental.layouts=true
+
 # We don't have to build a snap when we should use one from a
 # channel
 if [ -n "$SNAP_CHANNEL" ] ; then
