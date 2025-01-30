e3adbd0c4ddd589e24506a1c91b73c11b9ffb937
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-01-30 18:25:08 +0100

    Add nvidia-container-toolkit logging
    [skip ci]

--- .github/workflows/testflinger/scripts/test.sh
+++ .github/workflows/testflinger/scripts/test.sh
@@ -4,11 +4,11 @@ set -e
 # Test nvidia-smi
 smi_test() {
   . /etc/os-release
-
+  
   if [[ $ID == "ubuntu" ]]; then
-    sudo docker run --rm --runtime=nvidia --gpus all --env PATH="${PATH}:/var/lib/snapd/hostfs/usr/bin" ubuntu nvidia-smi || exit 1
+    sudo docker run --rm --runtime=nvidia --gpus all --env PATH="${PATH}:/var/lib/snapd/hostfs/usr/bin" ubuntu nvidia-smi || true
   elif [[ $ID == "ubuntu-core" ]]; then
-    sudo docker run --rm --runtime nvidia --gpus all ubuntu bash -c "/snap/docker/*/graphics/bin/nvidia-smi" || exit 1
+    sudo docker run --rm --runtime nvidia --gpus all ubuntu bash -c "/snap/docker/*/graphics/bin/nvidia-smi" || true
   else
     echo "Unexpected operating system ID: $ID"
     exit 1
@@ -17,10 +17,12 @@ smi_test() {
 
 # Test a vector addition sample workload
 vector_add_test() {
-  sudo docker run --rm --runtime=nvidia --gpus all nvcr.io/nvidia/k8s/cuda-sample:vectoradd-cuda10.2 || exit 1
+  sudo docker run --rm --runtime=nvidia --gpus all nvcr.io/nvidia/k8s/cuda-sample:vectoradd-cuda10.2
 }
 
 set -x
+sudo snap logs -n 100 docker.nvidia-container-toolkit
+
 smi_test
 
 vector_add_test
